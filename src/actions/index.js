2f23f80921bb5c752b4406c83e4f75b642d9e5e2
Markus Ånöstam <markus87@gmail.com>
2017-06-18 20:43:11 +0200

    Improve types and make prettier

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,23 +3,30 @@ import type {
   Paging,
   PlaylistTrack,
   SimplePlaylist,
-  User
+  User,
 } from '../types/spotify';
+import type { TrackWithMeta } from '../reducers/selectors';
 
 export type FetchTracksReqAction = {
   type: 'FETCH_TRACKS_REQ',
   playlist: SimplePlaylist,
-  offset: number
+  offset: ?number,
 };
 export type FetchTracksResAction = {
   type: 'FETCH_TRACKS_RES',
   pagedTracks: Paging<PlaylistTrack>,
-  playlistId: string
+  playlistId: string,
 };
 
 export type ScrambleTracksAction = {
   type: 'SCRAMBLE_TRACKS',
-  playlist: SimplePlaylist
+  playlist: SimplePlaylist,
+};
+
+export type CreatePlaylistsAction = {
+  type: 'CREATE_PLAYLISTS',
+  playlist: SimplePlaylist,
+  groupedTracks: Array<TrackWithMeta[]>,
 };
 
 export type Action =
@@ -34,37 +41,45 @@ export type Action =
 
 export const fetchUser = () => {
   return {
-    type: 'FETCH_USER_REQ'
+    type: 'FETCH_USER_REQ',
   };
 };
 export const receiveUser = (user: User): Action => ({
   type: 'FETCH_USER_RES',
-  user
+  user,
 });
 
 export const fetchPlaylists = (): Action => ({
-  type: 'FETCH_PLAYLISTS_REQ'
-});
-export const receivePlaylists = (
-  pagedPlaylists: Paging<SimplePlaylist>
-): Action => ({
-  type: 'FETCH_PLAYLISTS_RES',
-  pagedPlaylists
+  type: 'FETCH_PLAYLISTS_REQ',
 });
 
-export const fetchTracks = (playlist: SimplePlaylist, offset: ?number) => {
-  return {
-    type: 'FETCH_TRACKS_REQ',
-    playlist,
-    offset
-  };
-};
+export const fetchTracks = (
+  playlist: SimplePlaylist,
+  offset: ?number,
+): FetchTracksReqAction => ({
+  type: 'FETCH_TRACKS_REQ',
+  playlist,
+  offset,
+});
 
 export const scrambleTracks = (
-  playlist: SimplePlaylist
+  playlist: SimplePlaylist,
 ): ScrambleTracksAction => ({
   type: 'SCRAMBLE_TRACKS',
-  playlist
+  playlist,
+});
+
+export const createPlaylists = (playlist: SimplePlaylist, groupedTracks: Array<TrackWithMeta[]>): CreatePlaylistsAction => ({
+  type: 'CREATE_PLAYLISTS',
+  playlist,
+  groupedTracks,
 });
 
 export const unauthorized = (): Action => ({ type: 'REQUEST_UNAUTHORIZED' });
+
+export const receivePlaylists = (
+  pagedPlaylists: Paging<SimplePlaylist>,
+): Action => ({
+  type: 'FETCH_PLAYLISTS_RES',
+  pagedPlaylists,
+});
