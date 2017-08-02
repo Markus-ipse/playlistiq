13fa3703dac4e3482419c513c1dc562ebbd33cf9
Markus Ånöstam <markus87@gmail.com>
2017-08-02 09:58:14 +0200

    Add Confirm dialog component

---
+++ src/components/ConfirmDialog.js
@@ -0,0 +1,44 @@
+// @flow
+import React, { type Children } from 'react';
+
+type Props = {
+  children: Children,
+  title: string,
+  confirmText?: string,
+  cancelText?: string,
+  onConfirm: () => void,
+  onCancel: () => void,
+};
+
+const ConfirmDialog = ({
+  children,
+  title,
+  onConfirm,
+  onCancel,
+  confirmText = 'Save changes',
+  cancelText = 'Cancel',
+}: Props) =>
+  <div className="modal is-active">
+    <div className="modal-background" />
+    <div className="modal-card">
+      <header className="modal-card-head">
+        <p className="modal-card-title">
+          {title}
+        </p>
+        <button className="delete" onClick={onCancel} />
+      </header>
+      <section className="modal-card-body">
+        {children}
+      </section>
+      <footer className="modal-card-foot ps-justify-content-end">
+        <button className="button" onClick={onCancel}>
+          {cancelText}
+        </button>
+        <button className="button is-danger" onClick={onConfirm}>
+          {confirmText}
+        </button>
+      </footer>
+    </div>
+  </div>;
+
+export default ConfirmDialog;
