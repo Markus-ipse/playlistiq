475ac02810f310bdb42f5ea795fe92e4d6f49aa4
Markus Ånöstam <markus87@gmail.com>
2017-08-11 17:10:26 +0200

    Add helper for array flattening

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -5,3 +5,6 @@ export const isDev = () => process.env.NODE_ENV === 'development';
 
 export const getTotalPlayTime = (tracks: TrackWithMeta[]) =>
   msToFriendly(tracks.reduce((sum, item) => sum + item.track.duration_ms, 0));
+
+export const flatten = <T>(arr: T[][]): T[] =>
+  arr.reduce((acc, subArr) => acc.concat(subArr));
